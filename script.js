65a599b10992d57c3f6bb3dba26bd2297db500c1
rafahmarinho <rafael.marinho96@hotmail.com>
2020-06-17 21:43:55 -0300

    Atividade pronta pt.

---
+++ script.js
@@ -0,0 +1,13 @@
+alert ( "Olá! Seja bem vindo a minha página!" );
+alert ( "Vamos descobrir qual dentre 2 números é maior! Você me ajuda?" );
+
+let valorA = window.prompt ("Digite um número!");
+let valorB = window.prompt ("Agora digite outro número!");
+
+if (valorA > valorB) { 
+    alert ("O número "+ valorA + " é maior que o número "+ valorB +"." )
+}   else if (valorA < valorB){
+    alert ("O número "+ valorB + " é maior que o número "+ valorA + ".")
+}  else {
+    alert ("Os números que você digitou são iguais!")
+}
